26f09fa23ad0d410a4f58f16a599b911d1964b28
Spencer <spanglies@users.noreply.github.com>
2023-06-11 20:54:48 -0700

    Looks like this isn't easily swayed from requiring a full base path...

--- Build.sh
+++ Build.sh
@@ -1,4 +1,12 @@
 python Build.py
+if [[$CF_PAGES_BRANCH="master"]] || [[ -z "$CF_PAGES_URL" ]]
+then
+  cat base_url>>cat base_url >> config.toml
+else
+  echo "base_url = $CF_PAGES_URL" >> config.toml
+fi
+cat config.base.toml >> config.toml
+
 yarn build
 rm public/manifest.json
 cp -r public publish
